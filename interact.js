73c1fef9f5112859e7bbf1d423b2bc4e8220a788
Toheeb Alade <aladetoheeb5@gmail.com>
2024-10-05 22:42:41 +0100

    Interacting with deployed smart contracts using scripts or HTML

---
+++ interact.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const ethers = require('ethers')
+
+const RPC_URL = process.env.RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const CONTRACT_ADDRESS='0x8F63d939Df7201324C89b52D1E3FFaC8Ab2be39E'
+
+const abi = require('./abi.json')
+
+provider = new ethers.JsonRpcProvider(RPC_URL);
+signer = new ethers.Wallet(PRIVATE_KEY, provider)
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer)
+
+//console.log(contract)
+async function getNum() {
+    const num = await contract.retrieve()
+    console.log(num)
+}
+
+async function setNum() {
+    const tx = await contract.save(78);
+    await tx.wait()
+    console.log(tx.hash)
+}
+
+;(async () => {
+    await getNum();
+    // await setNum();
+    // await getNum();
+})().catch(console.error)
